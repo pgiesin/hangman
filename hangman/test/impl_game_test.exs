723d3130ce7d6c56a2d44a0b70a6213e3cf2f6c7
pgiesin <pgiesin@gmail.com>
2022-04-08 09:34:15 -0600

    Basic Game

---
+++ hangman/test/impl_game_test.exs
@@ -0,0 +1,28 @@
+defmodule HangmanImplGameTest do
+  use ExUnit.Case
+  alias Hangman.Impl.Game
+
+  test "new game returns structure" do
+    game = Game.new_game
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "new game returns correct word" do
+    game = Game.new_game("wombat")
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert game.letters == ["w", "o", "m", "b", "a", "t"]
+  end
+
+  test "state doesn't change if a game is won or lost" do
+    for state <- [:won, :lost] do
+      game = Game.new_game("wombat")
+      game = Map.put(game, :game_state, state)
+      { new_game, _tally } = Game.make_move(game, "x")
+
+      assert new_game == game
+    end
+  end
+end
